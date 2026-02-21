2cbfb8e00c0f09def22648fad92b5c6eb3e28810
Panom Michael <p.makuei@alustudent.com>
2026-02-21 11:02:35 +0300

    fixing errors

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,43 +5,43 @@
 For a given employee ID, returns information about
 his/her TODO list progress.
 """
-
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    # Check if employee ID is provided
     if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} emp_id", file=sys.stderr)
         sys.exit(1)
 
-    employee_id = sys.argv[1]
-
-    # API endpoints
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-
-    # Fetch user information
-    user_response = requests.get(user_url)
-    if user_response.status_code != 200:
+    try:
+        emp_id = int(sys.argv[1])
+    except ValueError:
+        print("the id must be vaild (int type)", file=sys.stderr)
         sys.exit(1)
 
-    user_data = user_response.json()
-    employee_name = user_data.get("name")
+    base = "https://jsonplaceholder.typicode.com"
 
-    # Fetch TODO list
-    todos_response = requests.get(todos_url)
-    if todos_response.status_code != 200:
+    url_response = requests.get(f"{base}/users/{emp_id}")
+    if url_response.status_code != 200:
         sys.exit(1)
+    user = url_response.json()
+    emp_name = user.get("name")
 
-    todos = todos_response.json()
+    todo_list_response = requests.get(
+        f"{base}/todos", params={"userId": emp_id})
+    if todo_list_response.status_code != 200:
+        sys.exit(1)
+    todo_list = todo_list_response.json()
 
-    total_tasks = len(todos)
-    done_tasks = [task for task in todos if task.get("completed") is True]
-    number_done = len(done_tasks)
+    success = [task for task in todo_list if task.get("completed") is True]
 
-    # Print required format
-    print(f"Employee {employee_name} is done with tasks({number_done}/{total_tasks}):")
+    f_line = (
+        f"Employee {emp_name} is done with tasks"
+        f"({len(success)}/{len(todo_list)}):"
+    )
+    print(f_line)
 
-    for task in done_tasks:
-        print("\t {}".format(task.get("title")))
+    for task in success:
+        title = task.get("title")
+        print("\t {}".format(title))
