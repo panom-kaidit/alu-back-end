3c6edb5ddca38177c8d0c723ae1220c47f07e941
Panom M <p.makuei@alustudent.com>
2026-02-22 03:00:36 -0800

    fixing errors

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,47 +1,38 @@
 #!/usr/bin/python3
 """
-0-gather_data_from_an_API.py
-
-For a given employee ID, returns information about
-his/her TODO list progress.
+Script that, using a REST API, returns information about
+an employee's TODO list progress.
 """
+
 import requests
 import sys
 
 
 if __name__ == "__main__":
+    # Check if employee ID is provided
     if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} emp_id", file=sys.stderr)
         sys.exit(1)
 
-    try:
-        emp_id = int(sys.argv[1])
-    except ValueError:
-        print("the id must be vaild (int type)", file=sys.stderr)
-        sys.exit(1)
+    employee_id = sys.argv[1]
 
-    base = "https://jsonplaceholder.typicode.com"
+    # API endpoints
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
 
-    url_response = requests.get(f"{base}/users/{emp_id}")
-    if url_response.status_code != 200:
-        sys.exit(1)
-    user = url_response.json()
-    emp_name = user.get("name")
+    # Fetch data
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todos_url)
 
-    todo_list_response = requests.get(
-        f"{base}/todos", params={"userId": emp_id})
-    if todo_list_response.status_code != 200:
-        sys.exit(1)
-    todo_list = todo_list_response.json()
+    # Convert JSON responses
+    user = user_response.json()
+    todos = todos_response.json()
 
-    success = [task for task in todo_list if task.get("completed") is True]
+    employee_name = user.get("name")
+    total_tasks = len(todos)
+    done_tasks = [task for task in todos if task.get("completed")]
 
-    f_line = (
-        f"Employee {emp_name} is done with tasks"
-        f"({len(success)}/{len(todo_list)}):"
-    )
-    print(f_line)
+    # Print required output
+    print(f"Employee {employee_name} is done with tasks({len(done_tasks)}/{total_tasks}):")
 
-    for task in success:
-        title = task.get("title")
-        print("\t {}".format(title))
+    for task in done_tasks:
+        print(f"\t {task.get('title')}")
